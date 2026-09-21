c3a984ba11d8ce8d50d3dd975a5d91e1c8016ae8
MSc Luis A. Hernández Elias <88979457+LuisHdezE@users.noreply.github.com>
2026-09-21 15:17:13 -0300

    feat(aok): package shared knowledge resources on Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,10 @@ android {
     buildConfig = true
   }
 
+  sourceSets {
+    getByName("main").assets.srcDir("../shared/src/commonMain/resources")
+  }
+
   testOptions { unitTests { isIncludeAndroidResources = true } }
   dependenciesInfo {
     includeInApk = false
